6e8c8271c93352f95fc64d596cda776519bcf1ca
Raluca G <ralucaguran@users.noreply.github.com>
2018-01-17 15:10:47 +0100

    leetcode solutions

---
+++ 278_first_bad_version.py
@@ -0,0 +1,26 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+def isBadVersion(version):
+    return True if version > 10 else False
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1,n 
+        first_bad = -1
+        while left <= right:
+            mid = left + (right-left)/2
+            if isBadVersion(mid):
+                right = mid - 1
+                first_bad = mid
+            else:
+                left = mid + 1
+        return first_bad
+
+sol = Solution()
+assert(sol.firstBadVersion(15)==11)
+
