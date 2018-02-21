57b1d94ea25d5a12f4e3f12c87ddc2bc64884b27
Raluca G <ralucaguran@users.noreply.github.com>
2018-02-21 11:09:26 +0100

    leetcode solutions

--- 451_sort_characters_by_frequency.cpp
+++ 451_sort_characters_by_frequency.cpp
@@ -8,15 +8,24 @@ using namespace std;
 class Solution {
     public:
         string frequencySort(string s) {
-            std::array<unsigned,128> freqs;
-            freqs.fill(0);
+            std::array<std::pair<char,unsigned>,128> freqs{{}};
             for (char ch:s) {
-                ++freqs[ch];
+                freqs[ch].first = ch;
+                ++freqs[ch].second;
             }
-            std::sort(s.begin(), s.end(), [&freqs](char x, char y) { 
-                    return (freqs[x] > freqs[y]) || (freqs[x] == freqs[y] && x < y); 
+            std::sort(freqs.begin(), freqs.end(), [](auto& x, auto& y) { 
+                    return (x.second > y.second) || (x.second == y.second && x.first < y.first); 
                     });
-            return s;
+            std::string sortedStr;
+            for (auto it = freqs.begin(); it != freqs.end(); ++it) {
+                if (it->first) {
+                    sortedStr.append(it->second, it->first);
+                }
+                else {
+                    break;
+                }
+            }
+            return sortedStr;
         }
 };
 
@@ -26,6 +35,8 @@ int main() {
     assert(sol.frequencySort("cccaaa") == "aaaccc");
     assert(sol.frequencySort("Aabb") == "bbAa");
     assert(sol.frequencySort("loveleetcode") == "eeeelloocdtv");
+    assert(sol.frequencySort("Aazz") == "zzAa");
+    assert(sol.frequencySort("2a554442f544asfasssffffasss") == "sssssssffffff44444aaaa55522");
     return 0;
 }
 
